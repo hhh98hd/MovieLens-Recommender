a7447f4f9e3502e15b1058f438717b08b5442441
Huy Hoang Hoang <u7671528@anu.edu.au>
2025-04-04 21:38:58 +1100

    Use one-hot encoding on user's occupation and gender columns
    -We avoid label encoding as a higher-labelled value does not necessary mean an occupation or gender is more impportant.
    -gender {F, M} -> gender_F and gender_M
    -occupation {artist, doctor, ...} -> occ_artist, occ_doctor, ...
    -Values of all encoded columns are either 1 (True) or 0 (False)

--- data/MovieLensDataset.py
+++ data/MovieLensDataset.py
@@ -1,22 +1,22 @@
-import datetime
-
 import pandas as pd
 
 class MovieLensDataset:
 
     def __init__(self, dir : str):
         # user_id | item_id | rating | timestamp
-        self._user_rating = pd.read_csv(dir + "u.data", sep="\t", header=None, names=["user_id", "item_id", "rating", "timestamp"])
+        self._user_rating_df = pd.read_csv(dir + "u.data", sep="\t", header=None, names=["user_id", "item_id", "rating", "timestamp"])
 
         # user id | age | gender | occupation | zip code
-        self._ser_info = pd.read_csv(dir + "u.user", sep="|", header=None, names=["user_id", "age", "gender", "occupation", "zip_code"])
-        
+        self._user_info_df = pd.read_csv(dir + "u.user", sep="|", header=None, names=["user_id", "age", "gender", "occupation", "zip_code"])
+        # One-hot encode the occupation and gender columns
+        self._user_info_df = pd.get_dummies(self._user_info_df, columns=['gender'])
+        self._user_info_df = pd.get_dummies(self._user_info_df, columns=['occupation'], prefix='occ')
+      
         # item id | movie title | release date | video release date || IMDb URL | unknown | Action | Adventure | Animation | Children's | Comedy | Crime | Documentary | Drama | Fantasy | Film-Noir | Horror | Musical | Mystery | Romance | Sci-Fi | Thriller | War | Western
-        self._movie_info = pd.read_csv(dir + "u.item", 
+        self._movie_info_df = pd.read_csv(dir + "u.item", 
                                 sep="|", header=None,
                                 names=["movie_id", "movie_title", "release_date", "url",
                                        "unknown", "Action", "Adventure", "Animation", "Children's", 
                                        "Comedy", "Crime", "Documentary", "Drama", "Fantasy", "Film-Noir", 
-                                       "Horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", "War", "Western"])
-        
+                                       "Horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", "War", "Western"])        
         
